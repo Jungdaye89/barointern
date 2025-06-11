b3fd53923d482b89533262cc0e7cc26020d96bb5
JUNG DAYE <jungyj0809@gmail.com>
2025-06-11 11:54:07 +0900

    feat: POST /login 엔드포인트 구현
    - 200 OK 상태로 TokenResponse 반환
    
    #3

--- src/main/java/com/sparta/barointern/controller/UserController.java
+++ src/main/java/com/sparta/barointern/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sparta.barointern.controller.dto.LoginRequest;
+import com.sparta.barointern.controller.dto.LoginResponse;
 import com.sparta.barointern.controller.dto.SignupRequest;
 import com.sparta.barointern.controller.dto.SignupResponse;
 import com.sparta.barointern.service.UserService;
@@ -23,5 +25,12 @@ public class UserController {
 
 		SignupResponse response = userService.signup(signupRequest);
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+
+		LoginResponse response = userService.login(loginRequest);
+		return ResponseEntity.ok(response);
 	}
 }
