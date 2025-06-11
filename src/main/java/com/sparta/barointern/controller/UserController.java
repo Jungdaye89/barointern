7fef81df0bf0a74402e2d25452724731bf3c6654
JUNG DAYE <jungyj0809@gmail.com>
2025-06-11 11:34:35 +0900

    feat: POST /signup 엔드포인트 매핑 - 201 Created 상태로 SignupResponse 반환
    #2

---
+++ src/main/java/com/sparta/barointern/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.sparta.barointern.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sparta.barointern.controller.dto.SignupRequest;
+import com.sparta.barointern.controller.dto.SignupResponse;
+import com.sparta.barointern.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+	public final UserService userService;
+
+	@PostMapping("/signup")
+	public ResponseEntity<SignupResponse> signup(@RequestBody SignupRequest signupRequest) {
+
+		SignupResponse response = userService.signup(signupRequest);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+}
